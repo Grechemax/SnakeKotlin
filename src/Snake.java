962d7bea8872f249b28fc669f791e9f0b6db1940
grechemax <grech@i.ua>
2019-10-29 11:19:19 +0200

    splited into methods. Drow still isn't working

--- src/Snake.java
+++ src/Snake.java
@@ -1,56 +1,108 @@
 import java.util.Scanner;
 
 public class Snake {
+    public static int x = 0;
+    public static int o = 0;
+
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         String snake;
 
-        System.out.println("Enter a string. Length: 9 characters min");
+        System.out.println("Enter a string of length 9:");
         snake = myObj.nextLine();
-        System.out.println("You inputted: " + snake);
         String[] arrOfStr = snake.split("");
+        System.out.println("You inputted: " + snake);
 
         // for better look
-        String[] beautifiedStr = snake.split("(?<=\\G.{" + 3 + "})");
+        String[] beautifiedStr = snake.split("(?<=\\G.{" + 3 + "})"); // split String for 3 symbols
         for (String b : beautifiedStr) {
             System.out.println(b);
         }
 
-        // if status is correct
-        if (snake.matches("(x|o|-)+")) {
-            //Diagonal 1
-            if (arrOfStr[0].equals("x") && arrOfStr[4].equals("x") && arrOfStr[8].equals("x")) {
-                System.out.println("Корректный статус, игра закончена, крестики  выиграли");
-            }
-            if (arrOfStr[0].equals("o") && arrOfStr[4].equals("o") && arrOfStr[8].equals("o")) {
-                System.out.println("Корректный статус, игра закончена, нолики  выиграли");
-            }
 
-            //Diagonal 2
-            if (arrOfStr[2].equals("x") && arrOfStr[4].equals("x") && arrOfStr[6].equals("x")) {
-                System.out.println("Корректный статус, игра закончена, крестики  выиграли");
+        for (String a : arrOfStr) {
+            if (a.equals("x")) {
+                x++;
             }
-            if (arrOfStr[2].equals("o") && arrOfStr[4].equals("o") && arrOfStr[6].equals("o")) {
-                System.out.println("Корректный статус, игра закончена, нолики  выиграли");
+            if (a.equals("o")) {
+                o++;
             }
+        }
 
-            //Horizontal 1, Horizontal 2, Horizontal 3
-            for (String b : beautifiedStr) {
-                if (b.matches("(ooo)")) {
-                    System.out.println("Корректный статус, игра закончена, нолики  выиграли");
-                };
-                if (b.matches("(xxx)")) {
-                    System.out.println("Корректный статус, игра закончена, крестики  выиграли");
-                };
-            }
-        } else if (snake.length() == 9 && !snake.matches("(x|o|-)+")) {
-            System.out.println("Правильный ввод, некорректный статус");
+        if (isInputValid(snake)) {
+            whoIsVinner(arrOfStr, beautifiedStr);
+        } else if (x == 4 && o == 4) {
+            System.out.println("ничья");
+        }
+    }// end of main method
+
+    static boolean isInputValid(String snake) {
+        if (snake.matches("(x|o|-)+") && snake.length() == 9) {
+            return isStatusValid();
         } else {
             System.out.println("Неправильный ввод");
+            return false;
         }
-    } // end of main method
+    }
+
+    static boolean isStatusValid() {
+        if (x > 0) {
+            System.out.println("Корректный статус");
+            return true;
+        } else {
+            System.out.println("Правильный ввод, некорректный статус");
+            return false;
+        }
+    }
+
+    static void whoIsVinner(String[] arrOfStr, String[] beautifiedStr) {
+        //Diagonal 1
+        if (arrOfStr[0].equals("x") && arrOfStr[4].equals("x") && arrOfStr[8].equals("x")) {
+            System.out.println("Корректный статус, игра закончена, крестики  выиграли");
+        }
+        if (arrOfStr[0].equals("o") && arrOfStr[4].equals("o") && arrOfStr[8].equals("o")) {
+            System.out.println("Корректный статус, игра закончена, нолики  выиграли");
+        }
+
+        //Diagonal 2
+        if (arrOfStr[2].equals("x") && arrOfStr[4].equals("x") && arrOfStr[6].equals("x")) {
+            System.out.println("Корректный статус, игра закончена, крестики  выиграли");
+        }
+        if (arrOfStr[2].equals("o") && arrOfStr[4].equals("o") && arrOfStr[6].equals("o")) {
+            System.out.println("Корректный статус, игра закончена, нолики  выиграли");
+        }
+        //Horizontal 1, Horizontal 2, Horizontal 3
+        for (String b : beautifiedStr) {
+            if (b.matches("(ooo)")) {
+                System.out.println("Корректный статус, игра закончена, нолики  выиграли");
+            }
+            if (b.matches("(xxx)")) {
+                System.out.println("Корректный статус, игра закончена, крестики  выиграли");
+            }
+        }
+
+        //Vertical 1
+        if (arrOfStr[0].equals("x") && arrOfStr[3].equals("x") && arrOfStr[6].equals("x")) {
+            System.out.println("Корректный статус, игра закончена, крестики  выиграли");
+        }
+        if (arrOfStr[0].equals("o") && arrOfStr[3].equals("o") && arrOfStr[6].equals("o")) {
+            System.out.println("Корректный статус, игра закончена, нолики  выиграли");
+        }
+       //Vertical 2
+        if (arrOfStr[1].equals("x") && arrOfStr[4].equals("x") && arrOfStr[7].equals("x")) {
+            System.out.println("Корректный статус, игра закончена, крестики  выиграли");
+        }
+        if (arrOfStr[1].equals("o") && arrOfStr[4].equals("o") && arrOfStr[7].equals("o")) {
+            System.out.println("Корректный статус, игра закончена, нолики  выиграли");
+        }
+        // Vertical 3
+        if (arrOfStr[2].equals("x") && arrOfStr[5].equals("x") && arrOfStr[8].equals("x")) {
+            System.out.println("Корректный статус, игра закончена, крестики  выиграли");
+        }
+        if (arrOfStr[2].equals("o") && arrOfStr[5].equals("o") && arrOfStr[8].equals("o")) {
+            System.out.println("Корректный статус, игра закончена, нолики  выиграли");
+        }
+    }
 }
-
-
 
 
