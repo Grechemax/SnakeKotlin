33d385bb31474340d2a94713b60ca0d71a97112c
max <grech@i.ua>
2019-11-22 18:46:55 +0200

    removed excel to test; added git ignore; statusCode int.

--- src/test/java/utils/Constant.java
+++ src/test/java/utils/Constant.java
@@ -3,6 +3,6 @@ package utils;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 
 public class Constant {
-    public static final String SHEET_PATH = "/Users/max/IdeaProjects/SnakeKotlin/excel/Book2.xlsx";
+    public static final String SHEET_PATH = "/Users/max/IdeaProjects/SnakeKotlin/src/test/excel/Book2.xlsx";
     public static String SHEET = "sheet1";
 }
